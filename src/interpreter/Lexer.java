b88ad280e807c5877a6277f4ab703deabd0ab06a
chenEini <chen.eini000@gmail.com>
2020-01-15 00:24:36 +0200

    creating lexer and parser functions

--- src/interpreter/Lexer.java
+++ src/interpreter/Lexer.java
@@ -1,5 +1,45 @@
 package interpreter;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Lexer {
 
+	private static Lexer lexerInstance = null;
+
+	public static Lexer getInstance() {
+		if (lexerInstance == null)
+			lexerInstance = new Lexer();
+		return lexerInstance;
+	}
+
+	public String[] lexer(String line) {
+
+		String[] tokens = line.split("(?<=([={}])|(bind))|(?=([={}]))");
+		StringBuilder builder = new StringBuilder();
+		Scanner sc;
+		String str1, str2;
+
+		for (String token : tokens) {
+			sc = new Scanner(token);
+			str1 = sc.next();
+			builder.append(str1);
+
+			while (sc.hasNext()) {
+				str2 = sc.next();
+				if (isFullExpression(str1, str2))
+					builder.append(",");
+				builder.append(str2);
+				str1 = str2;
+			}
+			builder.append(",");
+		}
+		return builder.toString().split(",");
+	}
+
+	private boolean isFullExpression(String str1, String str2) {
+		Pattern end = Pattern.compile(".*[\\w)\"]");
+		Pattern start = Pattern.compile("[\\w(\"].*");
+		return (end.matcher(str1).matches() && start.matcher(str2).matches());
+	}
 }
--- src/interpreter/Parser.java
+++ src/interpreter/Parser.java
@@ -1,5 +1,61 @@
 package interpreter;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import command.AssignmentCommand;
+import command.BindCommand;
+import command.Command;
+import command.ConnectCommand;
+import command.DisconnectCommand;
+import command.OpenServerCommand;
+import command.ReturnCommand;
+import command.VarCommand;
+import command.WhileCommand;
+
 public class Parser {
 
+	private static Parser parserInstance = null;
+	private static HashMap<String, Command> commandMap = new HashMap<String, Command>();
+
+	public static Parser getInstance() {
+		if (parserInstance == null) {
+			parserInstance = new Parser();
+			initializeCommandMap();
+		}
+		return parserInstance;
+	}
+
+	private static void initializeCommandMap() {
+		commandMap.put("openDataServer", new OpenServerCommand());
+		commandMap.put("connect", new ConnectCommand());
+		commandMap.put("var", new VarCommand());
+		commandMap.put("bind", new BindCommand());
+		commandMap.put("=", new AssignmentCommand());
+		commandMap.put("return", new ReturnCommand());
+		commandMap.put("disconnect", new DisconnectCommand());
+		commandMap.put("while", new WhileCommand());
+	}
+
+	public void parse(String[] line) {
+
+		List<String> lineList = Arrays.asList(line);
+
+		for (int i = 0; i < lineList.size(); i++) {
+			String key = lineList.get(i);
+
+			if (commandMap.containsKey(key)) {
+				if (key == "=" && i < lineList.size() - 1 && lineList.get(i + 1) == "bind") {
+					commandMap.get(lineList.get(i + 1)).doCommand(Arrays.asList(lineList.get(i + 2)));
+					lineList.remove(i + 1);
+				}
+				lineList.remove(i);
+				commandMap.get(key).doCommand(lineList);
+			} else {
+				// change variable to value
+				// if null do nothing
+			}
+		}
+	}
 }
