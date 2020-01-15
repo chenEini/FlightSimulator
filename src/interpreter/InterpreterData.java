035f7b93abf1c264929603120e2b976368553dd0
chenEini <chen.eini000@gmail.com>
2020-01-15 20:21:32 +0200

    add data storage for each interpreter

---
+++ src/interpreter/InterpreterData.java
@@ -0,0 +1,31 @@
+package interpreter;
+
+import java.util.HashMap;
+
+public class InterpreterData {
+
+	private HashMap<String, Double> symbolTable;
+	private HashMap<String, String> bindTable;
+
+	private InterpreterData() {
+		symbolTable = new HashMap<String, Double>();
+		bindTable = new HashMap<String, String>();
+	}
+
+	public void addSymbol(String sym, Double val) {
+		symbolTable.put(sym, val);
+	}
+
+	public Double getSymbol(String sym) {
+		return symbolTable.get(sym); // return null is symbol not exist
+	}
+
+	public void addBind(String sym, String val) {
+		bindTable.put(sym, val);
+	}
+
+	public String getBind(String sym) {
+		return bindTable.get(sym); // return null is symbol not exist
+	}
+
+}
