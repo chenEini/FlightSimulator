0c1f3e87c33128a1fc83495907b32ada86c59e92
chenEini <chen.eini000@gmail.com>
2020-01-16 20:41:26 +0200

    update open server command logic

--- src/command/OpenServerCommand.java
+++ src/command/OpenServerCommand.java
@@ -1,11 +1,28 @@
 package command;
 
+import server.ClientHandler;
+import server.FlightSimulatorClientHandler;
+import server.MySerialServer;
 import java.util.List;
 
 public class OpenServerCommand implements Command {
 
+	static MySerialServer server;
+
+	public static void closeServer() {
+		if (server != null)
+			server.stop();
+	}
+
 	@Override
 	public void doCommand(List<String> str) {
 
+		int port = Integer.parseInt(str.get(0));
+		int frequency = Integer.parseInt(str.get(1));
+
+		ClientHandler handler = new FlightSimulatorClientHandler(frequency);
+
+		server = new MySerialServer(port, handler);
+		server.start();
 	}
 }
