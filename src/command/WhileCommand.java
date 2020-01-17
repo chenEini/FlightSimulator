e9bb042a51c8b448d8c74cc2ba64102b4c4f8c5d
chenEini <chen.eini000@gmail.com>
2020-01-17 13:44:37 +0200

    fix the condition split regex

--- src/command/WhileCommand.java
+++ src/command/WhileCommand.java
@@ -1,73 +1,70 @@
 package command;
 
-import java.util.ArrayList;
 import java.util.List;
-
+import java.util.ArrayList;
 import expression.Calculator;
 
 public class WhileCommand implements Command {
 
-	private List<Command> commandLst;
-	private List<List<String>> paramsLst;
+	private List<Command> commandList;
+	private List<List<String>> paramsList;
 	private String condition;
 
 	public WhileCommand() {
-		super();
-		this.commandLst = new ArrayList<Command>();
-		this.paramsLst = new ArrayList<List<String>>();
-	}
-
-	@Override
-	public void doCommand(List<String> str) {
-
-		while (checkCondition()) {
-			for (int i = 0; i < commandLst.size(); i++) {
-				commandLst.get(i).doCommand(paramsLst.get(i));
-			}
-		}
+		this.commandList = new ArrayList<Command>();
+		this.paramsList = new ArrayList<List<String>>();
 	}
 
 	public void setCondition(String condition) {
 		this.condition = condition;
 	}
 
-	private boolean checkCondition() {
-		String[] split = condition.split("(\\+|-|\\*|\\/|=|>|<|>=|<=|==|&|\\||%|!|\\^|\\(|\\))");
-		Double p1 = Calculator.calc(split[0]);
-		Double p2 = Calculator.calc(split[2]);
-		boolean condVal;
-
-		switch (split[1]) {
-		case "==":
-			condVal = p1 == p2;
-			break;
-		case "<":
-			condVal = p1 < p2;
-			break;
-		case ">":
-			condVal = p1 > p2;
-			break;
-		case "<=":
-			condVal = p1 <= p2;
-			break;
-		case ">=":
-			condVal = p1 >= p2;
-			break;
-		case "!=":
-			condVal = p1 != p2;
-			break;
-		default:
-			condVal = false;
-		}
-		return condVal;
-	}
-
 	public void addCommand(Command c) {
-		commandLst.add(c);
+		commandList.add(c);
 	}
 
 	public void addParams(List<String> params) {
-		paramsLst.add(params);
+		paramsList.add(params);
 	}
 
+	@Override
+	public void doCommand(List<String> str) {
+		while (checkCondition()) {
+			for (int i = 0; i < commandList.size(); i++) {
+				commandList.get(i).doCommand(paramsList.get(i));
+			}
+		}
+	}
+
+	private boolean checkCondition() {
+		boolean conditionVal;
+
+		String[] split = condition.split("((?<=<|>|==|!=|<=|>=)|(?=<|>|==|!=|<=|>=))");
+		Double p1 = Calculator.calc(split[0]);
+		Double p2 = Calculator.calc(split[2]);
+
+		switch (split[1]) {
+		case "<":
+			conditionVal = p1 < p2;
+			break;
+		case ">":
+			conditionVal = p1 > p2;
+			break;
+		case "<=":
+			conditionVal = p1 <= p2;
+			break;
+		case ">=":
+			conditionVal = p1 >= p2;
+			break;
+		case "==":
+			conditionVal = p1 == p2;
+			break;
+		case "!=":
+			conditionVal = p1 != p2;
+			break;
+		default:
+			conditionVal = false;
+		}
+		return conditionVal;
+	}
 }
